6fd9baecb740500cd5ceaa90e79c124ea08dd626
Kerolos Farag <72260733+ksfarag@users.noreply.github.com>
2023-01-31 01:54:16 -0330

    Add pickup audio to coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag=="Player")
         {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
